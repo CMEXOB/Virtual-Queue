de5aa2f94acf77f19b73115c3b040bbe64fd2ad7
Skripko <games.03@inbox.ru>
2023-06-09 12:26:00 +0300

    Server initialization

---
+++ Server/src/main/java/com/QueueServer.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueServer {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueServer.class, args);
+    }
+}
