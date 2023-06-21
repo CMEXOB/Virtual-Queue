40ea33c2ad4bec1f06bde86304e6eccb59dd90c9
Skripko <games.03@inbox.ru>
2023-06-21 21:37:10 +0300

    add bootstrap to queue

--- client/src/components/ItemList.jsx
+++ client/src/components/ItemList.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import Item from "./Item";
+import {Card, Container, ListGroup} from "react-bootstrap";
 
 const ItemList = ({queue}) => {
     return (
-        <div>
-            {queue.map((item) =>
-                <Item item={item}/>
-            )}
-        </div>
+        <Container className="item-list position-relative">
+            <Card>
+                <Card.Body>
+                    <Card.Title className={'text-center'}>Queue</Card.Title>
+                </Card.Body>
+            </Card>
+            <ListGroup as="ul" variant="primary">
+                    {queue.map((item) =>
+                        <ListGroup.Item as="li" >{item.number+'.\t'+item.name}</ListGroup.Item>
+                    )}
+            </ListGroup>
+        </Container>
     );
 };
 
 export default ItemList;
+
